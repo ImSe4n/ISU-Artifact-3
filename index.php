d1aa899f06e1fcad1be9646fe3db94a3db313b1a
Sean Nie <montrealcanadiens12310@gmail.com>
2024-03-02 11:25:35 -0500

    fixed budget management, added icons

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 <head>
   <title>ISU Part 3</title>
   <link href="style.css" rel="stylesheet" type="text/css"/>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+  />
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap"
+    rel="stylesheet"
+  />
 </head>
 <body>
   <div class="grid-container">
@@ -48,118 +57,62 @@
       </div>
 
       <div class="content content1" style="display: none;">
-      <div class="top">
-            <div class="budget">
-                <div class="budget__title">
-                    Available Budget in <span class="budget__title--month">%Month%</span>:
-                </div>
-                
-                
-                
-                <div class="budget__value">+ 0</div>
-                
-                <div class="budget__income clearfix">
-                    <div class="budget__income--text">Income</div>
-                    <div class="right">
-                        <div class="budget__income--value">+ 0</div>
-                        <div class="budget__income--percentage">&nbsp;</div>
-                    </div>
-                </div>
-                
-                <div class="budget__expenses clearfix">
-                    <div class="budget__expenses--text">Expenses</div>
-                    <div class="right clearfix">
-                        <div class="budget__expenses--value">- 0</div>
-                        <div class="budget__expenses--percentage">45%</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        
-        
-        <div class="bottom">
-            <div class="add">
-                <div class="add__container">
-                    <select class="add__type">
-                        <option value="inc" selected>+</option>
-                        <option value="exp">-</option>
-                    </select>
-                    <input type="text" class="add__description" placeholder="Add description">
-                    <input type="number" class="add__value" placeholder="Value">
-					
-                    <input type="submit" class="add__btn" type='button'>
-                </div>
-            </div>
-            
-            <div class="container clearfix">
-                <div class="income">
-                    <h2 class="icome__title">Income</h2>
-                    
-                    <div class="income__list">
-                       
-                        <!--
-                        <div class="item clearfix" id="income-0">
-                            <div class="item__description">Salary</div>
-                            <div class="right clearfix">
-                                <div class="item__value">+ 2,100.00</div>
-                                <div class="item__delete">
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="item clearfix" id="income-1">
-                            <div class="item__description">Sold car</div>
-                            <div class="right clearfix">
-                                <div class="item__value">+ 1,500.00</div>
-                                <div class="item__delete">
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                        -->
-                        
-                    </div>
-                </div>
-                
-                
-                
-                <div class="expenses">
-                    <h2 class="expenses__title">Expenses</h2>
-                    
-                    <div class="expenses__list">
-                       
-                        <!--
-                        <div class="item clearfix" id="expense-0">
-                            <div class="item__description">Apartment rent</div>
-                            <div class="right clearfix">
-                                <div class="item__value">- 900.00</div>
-                                <div class="item__percentage">21%</div>
-                                <div class="item__delete">
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-                                </div>
-                            </div>
-                        </div>
+      <p class="title">Budget Management</p>
+            <div class="sub-container">
+              <!-- Budget -->
+              <div class="total-amount-container">
+                <h3>Budget</h3>
+                <p class="hide error" id="budget-error">
+                  Value cannot be empty or negative
+                </p>
+                <input
+                  type="number"
+                  id="total-amount"
+                  placeholder="Enter Total Amount"
+                />
+                <button class="submit" id="total-amount-button">Set Budget</button>
+              </div>
 
-                        <div class="item clearfix" id="expense-1">
-                            <div class="item__description">Grocery shopping</div>
-                            <div class="right clearfix">
-                                <div class="item__value">- 435.28</div>
-                                <div class="item__percentage">10%</div>
-                                <div class="item__delete">
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                        -->
-                        
-                    </div>
-                </div>
+              <!-- Expenditure -->
+              <div class="user-amount-container">
+                <h3>Expenses</h3>
+                <p class="hide error" id="product-title-error">
+                  Values cannot be empty
+                </p>
+                <input
+                  type="text"
+                  class="product-title"
+                  id="product-title"
+                  placeholder="Enter Title of Product"
+                />
+                <input
+                  type="number"
+                  id="user-amount"
+                  placeholder="Enter Cost of Product"
+                />
+                <button class="submit" id="check-amount">Check Amount</button>
+              </div>
             </div>
-            
-            
-        </div>
+            <!-- Output -->
+            <div class="output-container flex-space">
+              <div>
+                <p>Total Budget</p>
+                <span id="amount">0</span>
+              </div>
+              <div>
+                <p>Expenses</p>
+                <span id="expenditure-value">0</span>
+              </div>
+              <div>
+                <p>Balance</p>
+                <span id="balance-amount">0</span>
+              </div>
+            </div>
+          <!-- List -->
+          <div class="list">
+            <h3>Expense List</h3>
+            <div class="list-container" id="list"></div>
+          </div>
       </div>
 
       <div class="content content2" style="display: none;">
